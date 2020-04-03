6708ae00a8e9db391b3278eb23f097a8bec3107f
Germanicus <dawdud12@gmail.com>
2020-04-04 01:40:10 +0200

    add weeks

---
+++ Assets/scripts/TimeManagment.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManagment : MonoBehaviour
+{
+    public GameTime timeScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeScript = GameObject.Find("GameObject").GetComponent<GameTime>();
+    }
+
+    // Update is called once per frame
+   void Update()
+    {
+       int hours= timeScript.GetHours();
+      
+        //Debug.Log(hours);
+        Debug.Log(timeScript.GetWeek());
+
+    }
+}
--- Assets/scripts/gameTime.cs
+++ Assets/scripts/gameTime.cs
@@ -1,16 +1,27 @@
 ﻿using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class gameTime : MonoBehaviour
+public class GameTime : MonoBehaviour
 {
     // Start is called before the first frame update\
     private float day;
+    private float week;
 
-    public Text timeText;
+    //public Text timeText;
+    string hoursString;
+    int nextday;
+    int nextWeek;
+    string minutesString;
+    float dayNormalized;
+    float minutesPerHour = 60f;
+    float hoursPerDay = 24f;
+   
 
-    private const float REAL_SECONDS_PER_INGAME_DAY = 1000F;
+    private const float REAL_SECONDS_PER_INGAME_DAY = 2F;
+    private const float REAL_SECONDS_PER_INGAME_WEEK = REAL_SECONDS_PER_INGAME_DAY * 7;
     void Start()
     {
         
@@ -19,18 +30,58 @@ public class gameTime : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        day += Time.deltaTime / REAL_SECONDS_PER_INGAME_DAY;
-        float dayNormalized = day % 1f;
-        float hoursPerDay = 24f;
-        string dayString = Mathf.Floor(day).ToString();
-        float minutesPerHour = 60f;
-        string hoursString = Mathf.Floor(dayNormalized * hoursPerDay).ToString();
-       
-        string minutesString = Mathf.Floor(((dayNormalized * hoursPerDay) % 1f) * minutesPerHour).ToString("00");
-        timeText.text = hoursString + ":" + minutesString + " day: " + dayString ;
-        
-
+        setGameTime();
 
 
     }
+    public void setGameTime()
+    {
+        day += Time.deltaTime / REAL_SECONDS_PER_INGAME_DAY;
+        week += Time.deltaTime / REAL_SECONDS_PER_INGAME_WEEK;
+        float dayNormalized = day % 1f;
+      
+        
+       // Debug.Log(dayNormalized);
+        string dayString = Mathf.Floor(day).ToString();
+       
+        hoursString = Mathf.Floor(dayNormalized * hoursPerDay).ToString();
+        
+        minutesString = Mathf.Floor(((dayNormalized * hoursPerDay) % 1f) * minutesPerHour).ToString("00");
+       
+        
+
+    }
+    public int GetHours()
+    {
+        return int.Parse(hoursString);
+    }
+    public int GetMinutes()
+    {
+       return  int.Parse(minutesString);
+    }
+    public int GetWeek()
+    {
+        return (int)week;
+    }
+    public void setTimerMinutes(int now)
+    {
+        TimeSpan mints = TimeSpan.FromSeconds(300);
+        mints = mints.Subtract(TimeSpan.FromSeconds(int.Parse(minutesString)));
+        if (mints.Seconds == 0)
+        {
+           // Debug.Log("End");
+        }
+           
+    }
+    public void setTimer(int now)
+    {
+        TimeSpan end = new TimeSpan(22, 0, 0);
+        
+        TimeSpan ts = new TimeSpan(int.Parse(hoursString), 0,0);
+        
+           
+
+        
+    }
+    
 }
